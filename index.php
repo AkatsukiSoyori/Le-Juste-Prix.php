91d42ab52528a9127b94c4f1c991e0898821f611
Riyaiki <k1l14n.gr1gn3t@gmail.com>
2023-08-03 10:45:48 +0200

    exo 10 + index

--- index.php
+++ index.php
@@ -42,12 +42,37 @@ while ($randomNumber != 2) {
 // Une fois que le chiffre aléatoire est égal à 2, afficher le message final
 echo "Le chiffre aléatoire est maintenant 2<br>";
 
-// Déclaration du tableau contenant les villes
-$villes = ['Paris', 'Marseille', 'Reims', 'Charleville'];
+// Déclaration du tableau contenant les villes avec les informations
+$villes = [
+    [
+        'ville' => 'Paris',
+        'habitants' => '2,161 millions',
+        'superficie' => '104,5',
+        'altitude' => '35',
+    ],
+    [
+        'ville' => 'Marseille',
+        'habitants' => '861 635',
+        'superficie' => '240,6',
+        'altitude' => '242',
+    ],
+    [
+        'ville' => 'Reims',
+        'habitants' => '184 076',
+        'superficie' => '46,9',
+        'altitude' => '95',
+    ],
+    [
+        'ville' => 'Charleville',
+        'habitants' => '47 847',
+        'superficie' => '31,44',
+        'altitude' => '228',
+    ],
+];
 
-// Affichage de chaque ville à l'aide d'une boucle for
-for ($i = 0; $i < count($villes); $i++) {
-    echo $villes[$i] . "<br>";
+// Affichage de chaque ville à l'aide d'une boucle foreach
+foreach ($villes as $infoVille) {
+    echo "La ville de " . $infoVille['ville'] . " a " . $infoVille['habitants'] . " habitants, pour une superficie de " . $infoVille['superficie'] . " km2, et une altitude de " . $infoVille['altitude'] . " m.<br>";
 }
 
 ?>
