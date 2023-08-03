79e11777280a619a8b0e02d551ce0b58ffefa03a
Riyaiki <k1l14n.gr1gn3t@gmail.com>
2023-08-03 09:56:45 +0200

    index.php 3 update

--- index.php
+++ index.php
@@ -5,8 +5,11 @@
 </head>
 <body>
 <?php
-// Déclaration de la variable contenant le message
+// Déclaration de la variable contenant le message principal
 $message = "Hello World !!";
+
+// Concaténation de "2023" à la variable $message
+$message .= " 2023";
 
 // Affichage du contenu de la variable
 echo $message;
